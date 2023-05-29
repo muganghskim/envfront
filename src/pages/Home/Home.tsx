e739f296f5d2c798b8741a21183e14c22f4a6cfc
hskim <rhgustmfrh@naver.com>
2023-05-29 18:18:13 +0900

    home update

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
+import '../../assets/scss/home.css';
 
 const Home: React.FC = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="Home">
+      <video src={process.env.PUBLIC_URL + '/img/env1.mp4'} autoPlay muted loop></video>
     </div>
   );
 };
 
 export default Home;
+
